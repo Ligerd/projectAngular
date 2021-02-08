8f532f04d8ab1d3a9e33b35c73ab53231108832c
Aliaksandr Karolik <aliaksandr.karolik@computaris.com>
2021-02-08 11:48:20 +0100

    One more try from start

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,9 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
-import { FavoriteItemsComponent } from "./favorite-items/favorite-items.component";
-import { ShopItemDetailComponent } from "./shop/shop-item-detail/shop-item-detail.component";
-import { ShopStartComponent } from "./shop/shop-start/shop-start.component";
-import { ShopComponent } from "./shop/shop.component";
+
 
 
 const appRouts: Routes = [
-    {path: '', redirectTo: '/shop',pathMatch: 'full'},
-    {path: 'shop', component: ShopComponent, children:[
-        {path: '', component: ShopStartComponent},
-        {path: ':id', component: ShopItemDetailComponent},
-    ]},
-    {path: 'favorite', component: FavoriteItemsComponent}
 ];
 @NgModule({
     imports: [RouterModule.forRoot(appRouts)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,35 +4,20 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { DropdownDirective } from './shared/dropdown.directive';
-import { ShopItemComponent } from './shop/shop-item/shop-item.component';
-import { ShopListComponent } from './shop/shop-list/shop-list.component';
 
-import { ShopComponent } from './shop/shop.component';
-import { ShopStartComponent } from './shop/shop-start/shop-start.component';
-import { ShopService } from './shop/shop.service';
-import { ShopItemDetailComponent } from './shop/shop-item-detail/shop-item-detail.component';
-import { FavoriteItemsComponent } from './favorite-items/favorite-items.component';
-import { FavoriteService } from './favorite-items/favorite.service';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    DropdownDirective,
-    ShopComponent,
-    ShopListComponent,
-    ShopItemComponent,
-    ShopStartComponent,
-    ShopItemDetailComponent,
-    FavoriteItemsComponent
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [ShopService, FavoriteService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
